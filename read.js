47bdf670c006504cbaec5470f2c08a27e1f346cd
arifsatriya <98566695+arifsatriya@users.noreply.github.com>
2024-12-03 13:38:43 +0700

    Create read.js

---
+++ read.js
@@ -0,0 +1,13 @@
+const db = require("./db_datasiswa"); // digunakan untuk menuliskan nama database milik masing-masing
+const sql = "SELECT * FROM pelanggan";
+
+db.query(sql, function (err, result) {
+    if (err) throw err;
+    
+    // gunakan perulangan untuk menampilkan data
+    console.log(`ID \t NAMA \t\t\t ALAMAT`);
+    console.log(`----------------------------------------------------------`);
+    result.forEach((pelanggan) => {
+        console.log(`${pelanggan.id} \t ${pelanggan.nama} \t\t\t ${pelanggan.alamat}`);
+    });
+});
